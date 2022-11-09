556aceffe11a5b64fe42dca0103d0f9807dd828e
Oleg Maximov <maximov@a2design.biz>
2022-11-10 00:10:37 +0600

    Fixed typo

--- app/Http/Requests/StoreCarRequest.php
+++ app/Http/Requests/StoreCarRequest.php
@@ -26,7 +26,7 @@ class StoreCarRequest extends FormRequest
     public function rules()
     {
         return [
-            '*name' => 'required|string|max:255',
+            '*.name' => 'required|string|max:255',
         ];
     }
 
