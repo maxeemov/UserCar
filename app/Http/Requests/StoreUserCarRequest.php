28bbf7ff508bfeee54d6149805d7c6718023fea2
Oleg Maximov <maximov@a2design.biz>
2022-11-10 00:41:47 +0600

    Created StoreUserCarRequest

---
+++ app/Http/Requests/StoreUserCarRequest.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Contracts\Validation\Validator;
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Http\Exceptions\HttpResponseException;
+
+class StoreUserCarRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            '*.user_id'   => 'required|numeric|unique:users_cars',
+            '*.car_id'    => 'required|numeric|unique:users_cars',
+        ];
+    }
+
+    /**
+     * @param Validator $validator
+     * @return void
+     */
+    public function failedValidation(Validator $validator)
+    {
+        throw new HttpResponseException(response()->json([
+            'success'   => false,
+            'message'   => 'Validation errors',
+            'data'      => $validator->errors()
+        ], 422));
+    }
+}
