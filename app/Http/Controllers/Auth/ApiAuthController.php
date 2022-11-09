88eb2d88dc9c9e8da796240d67056824ed332cd9
Oleg Maximov <maximov@a2design.biz>
2022-11-09 17:43:40 +0600

    Added auth methods

--- app/Http/Controllers/Auth/ApiAuthController.php
+++ app/Http/Controllers/Auth/ApiAuthController.php
@@ -3,8 +3,60 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\LoginRequest;
+use App\Http\Requests\StoreUserRequest;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
+use App\Models\User;
 
 class ApiAuthController extends Controller
 {
-    //
+    /**
+     * @param LoginRequest $request
+     * @return \Illuminate\Contracts\Foundation\Application|\Illuminate\Contracts\Routing\ResponseFactory|\Illuminate\Http\Response
+     */
+    public function login(LoginRequest $request)
+    {
+        $user = User::where('email', $request->email)->first();
+
+        if ($user && Hash::check($request->password, $user->password)) {
+            $token = $user->createToken('Laravel Password Grant Client')->accessToken;
+
+            $response = ['success' => true, 'data' => ['token' => $token]];
+            return response($response, 200);
+        }
+
+        $response = ['success' => false, "message" => 'Email or password is no correct.'];
+        return response($response, 422);
+    }
+
+    /**
+     * @param Request $request
+     * @return \Illuminate\Contracts\Foundation\Application|\Illuminate\Contracts\Routing\ResponseFactory|\Illuminate\Http\Response
+     */
+    public function logout (Request $request)
+    {
+        $token = $request->user()->token();
+        $token->revoke();
+
+        $response = ['success' => true, 'message' => 'You have been successfully logged out.'];
+        return response($response, 200);
+    }
+
+    /**
+     * @param StoreUserRequest $request
+     * @return \Illuminate\Contracts\Foundation\Application|\Illuminate\Contracts\Routing\ResponseFactory|\Illuminate\Http\Response
+     */
+    public function store(StoreUserRequest $request)
+    {
+        $request['password'] = Hash::make($request->password);
+        $request['remember_token'] = Str::random(10);
+
+        $user = User::create($request->toArray());
+        $token = $user->createToken('Laravel Password Grant Client')->accessToken;
+
+        $response = ['success' => true, 'data' => ['token' => $token]];
+        return response($response, 200);
+    }
 }
