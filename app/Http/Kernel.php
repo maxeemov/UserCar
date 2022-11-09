af6ec662bfa4ab836ee1ee02c2a7c41cb5538e06
Oleg Maximov <maximov@a2design.biz>
2022-11-09 19:06:44 +0600

    Added middleware class to kernel

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -67,5 +67,6 @@ class Kernel extends HttpKernel
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
         'json.response' => \App\Http\Middleware\JsonResponse::class,
         'cors' => \App\Http\Middleware\Cors::class,
+        'admin' => \App\Http\Middleware\AdminAuth::class,
     ];
 }
